59d6627b61286868b7f10b47b4daa3ace56082b8
MrBCave <ceb@whs.school.nz>
2025-10-29 16:38:46 +1300

    Lesson 3: Adding Items

--- src/main/java/net/cavebr/bradleysschoolmod/BradleysSchoolMod.java
+++ src/main/java/net/cavebr/bradleysschoolmod/BradleysSchoolMod.java
@@ -1,5 +1,6 @@
 package net.cavebr.bradleysschoolmod;
 
+import net.cavebr.bradleysschoolmod.item.ModItems;
 import org.slf4j.Logger;
 
 import com.mojang.logging.LogUtils;
@@ -45,6 +46,9 @@ public class BradleysSchoolMod {
 
         // Register ourselves for server and other game events we are interested in.
         NeoForge.EVENT_BUS.register(this);
+
+        ModItems.register(modEventBus);
+
         // Register the item to a creative tab
         modEventBus.addListener(this::addCreative);
         // Register our mod's ModConfigSpec so that FML can create and load the config file for us
@@ -57,7 +61,9 @@ public class BradleysSchoolMod {
 
     // Add the example block item to the building blocks tab
     private void addCreative(BuildCreativeModeTabContentsEvent event) {
-
+        if (event.getTabKey() == CreativeModeTabs.INGREDIENTS) {
+            event.accept(ModItems.GREENSTONE);
+        }
     }
 
     // You can use SubscribeEvent and let the Event Bus discover methods to call
---
+++ src/main/java/net/cavebr/bradleysschoolmod/item/ModItems.java
@@ -0,0 +1,20 @@
+package net.cavebr.bradleysschoolmod.item;
+
+import net.cavebr.bradleysschoolmod.BradleysSchoolMod;
+import net.minecraft.world.item.Item;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.neoforge.registries.DeferredItem;
+import net.neoforged.neoforge.registries.DeferredRegister;
+
+public class ModItems {
+    public static final DeferredRegister.Items ITEMS = DeferredRegister.createItems(BradleysSchoolMod.MODID);
+
+    public static final DeferredItem<Item> GREENSTONE =
+            ITEMS.register("greenstone", () -> new Item(new Item.Properties()));
+
+
+    public static void register(IEventBus eventBus) {
+        ITEMS.register(eventBus);
+    }
+
+}
